c7b4d99b4471999f504397967d6ca19021657ed6
Henock Tesfaye <henock.tesfaye@ahadoo.com>
2020-08-30 17:53:55 +0300

    Fleshed out the Account schema

--- account/models.js
+++ account/models.js
@@ -1,6 +1,28 @@
 var mongoose = require('mongoose');
 
-var accountSchema = new mongoose.Schema({});
+var accountSchema = new mongoose.Schema({
+  uid: {
+    type: String,
+    match: /^\w{28}$/,
+    index: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    match: /^\+\d{12}$/,
+    index: true,
+    unique: true,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  /* userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: 'User',
+  }, */
+});
 
 var Account = mongoose.model('Account', accountSchema);
 
