0d560bcad2f42c592c83dd36136f8263329d38ab
Henock Tesfaye <henock.tesfaye@ahadoo.com>
2020-09-02 09:19:38 +0300

    Removed JavaScript files

--- src/user/routes.ts
+++ src/user/routes.ts
@@ -6,10 +6,9 @@ import * as firebaseMiddleware from '../account/middlewares';
 
 const router = Router();
 
-router.get('/:id', asyncHandler(controllers.get));
-
 router.use('/', firebaseMiddleware.auth);
 router.get('/mine', asyncHandler(controllers.mine));
+router.get('/:id', asyncHandler(controllers.get));
 router.put('/follow/:id', asyncHandler(controllers.follow));
 router.put('/unfollow/:id', asyncHandler(controllers.unfollow));
 
